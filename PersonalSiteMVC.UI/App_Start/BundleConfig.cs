bb544223cd9bfbea9d16826ab3f523d9fcbf0087
Wes Nelson <WesNelson10@outlook.com>
2021-04-23 12:54:05 -0500

    Trying to get my bg image to show up.

--- PersonalSiteMVC.UI/App_Start/BundleConfig.cs
+++ PersonalSiteMVC.UI/App_Start/BundleConfig.cs
@@ -12,19 +12,19 @@ namespace PersonalSiteMVC.UI
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/jquery.validate*"));           
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/vendor/icofont/icofont.min.css",
+                      "~/Content/vendor/remixicon/remixicon.css",
+                      "~/Content/vendor/owl.carousel/assets/owl.carousel.min.css",
+                      "~/Content/vendor/boxicons/css/boxicons.min.css",
+                      "~/Content/vendor/venobox/venobox.css",
+                      "~/Content/vendor/bootstrap/css/style.css"));
         }
     }
 }
